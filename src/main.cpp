83c02a261d678d809323a0341f7d6704fa91731a
JoacoSerra <joaquin_serra@hotmail.com>
2025-08-04 00:33:40 -0300

    Clean code, agregada linea para MQ7/monoxido, cambio de iconos

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,22 @@
 #include <TFT_eSPI.h>
 #include "weather_images.h"
 #include <WiFi.h>
-//#include <HTTPClient.h>
-//#include <ArduinoJson.h>
 #include <DHT.h>
+#include <time.h>
 
 // Replace with your network credentials
 const char* ssid = "Fibertel WiFi867 2.4GHz";
 const char* password = "0043559168";
 
-// Replace with the latitude and longitude to where you want to get the weather
-String latitude = "-38.73";
-String longitude = "-62.17";
 // Enter your location
 String location = "Bahia Blanca";
-// Type the timezone you want to get the time for
-String timezone = "America/Buenos_Aires";
 
 // Store date and time
 String current_date;
-String last_weather_update;
+//String last_weather_update;
 String temperature;
 String humidity;
+String monoxide;
 int is_day;
 int weather_code = 0;
 String weather_description;
@@ -45,8 +40,6 @@ String weather_description;
 DHT dht(DHTPIN, DHTTYPE);
 
 // Screen dimensions
-//#define SCREEN_WIDTH 240
-//#define SCREEN_HEIGHT 320
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 480
 
@@ -59,6 +52,7 @@ void get_weather_data();
 void log_print(lv_log_level_t level, const char * buf);
 static void timer_cb(lv_timer_t * timer);
 void lv_create_main_gui(void);
+String get_formatted_datetime();
 
 static lv_obj_t * weather_image;
 static lv_obj_t * text_label_date;
@@ -66,6 +60,7 @@ static lv_obj_t * text_label_temperature;
 static lv_obj_t * text_label_humidity;
 static lv_obj_t * text_label_weather_description;
 static lv_obj_t * text_label_time_location;
+static lv_obj_t * text_label_ppm; // Label for monoxide value
 
 void setup() {
   String LVGL_Arduino = String("LVGL Library Version: ") + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
@@ -83,6 +78,9 @@ void setup() {
   }
   Serial.print("\nConnected to Wi-Fi network with IP Address: ");
   Serial.println(WiFi.localIP());
+
+  configTzTime("GMT+3", "pool.ntp.org", "time.nist.gov");
+
   
   // Start LVGL
   lv_init();
@@ -114,8 +112,9 @@ void lv_create_main_gui(void) {
   LV_IMAGE_DECLARE(image_weather_night);
   LV_IMAGE_DECLARE(image_weather_temperature);
   LV_IMAGE_DECLARE(image_weather_humidity);
+  LV_IMAGE_DECLARE(image_monoxide);
 
-  // Get the weather data from open-meteo.com API
+  // Get weather data from DHT sensor
   get_weather_data();
 
   weather_image = lv_image_create(lv_screen_active());
@@ -125,47 +124,58 @@ void lv_create_main_gui(void) {
 
   text_label_date = lv_label_create(lv_screen_active());
   lv_label_set_text(text_label_date, current_date.c_str());
-  lv_obj_align(text_label_date, LV_ALIGN_CENTER, 70, -70);
+  lv_obj_align(text_label_date, LV_ALIGN_CENTER, 70, -125);
   lv_obj_set_style_text_font((lv_obj_t*) text_label_date, &lv_font_montserrat_26, 0);
   lv_obj_set_style_text_color((lv_obj_t*) text_label_date, lv_palette_main(LV_PALETTE_TEAL), 0);
 
+  // Temperature Icon
   lv_obj_t * weather_image_temperature = lv_image_create(lv_screen_active());
   lv_image_set_src(weather_image_temperature, &image_weather_temperature);
-  lv_obj_align(weather_image_temperature, LV_ALIGN_CENTER, 30, -25);
+  lv_obj_align(weather_image_temperature, LV_ALIGN_CENTER, 30, -60);
+
   text_label_temperature = lv_label_create(lv_screen_active());
   lv_label_set_text(text_label_temperature, String("      " + temperature + degree_symbol).c_str());
-  lv_obj_align(text_label_temperature, LV_ALIGN_CENTER, 70, -25);
+  lv_obj_align(text_label_temperature, LV_ALIGN_CENTER, 95, -60);
   lv_obj_set_style_text_font((lv_obj_t*) text_label_temperature, &lv_font_montserrat_22, 0);
 
+  // Humidity Icon
   lv_obj_t * weather_image_humidity = lv_image_create(lv_screen_active());
   lv_image_set_src(weather_image_humidity, &image_weather_humidity);
-  lv_obj_align(weather_image_humidity, LV_ALIGN_CENTER, 30, 20);
+  lv_obj_align(weather_image_humidity, LV_ALIGN_CENTER, 30, 15);
+
   text_label_humidity = lv_label_create(lv_screen_active());
   lv_label_set_text(text_label_humidity, String("   " + humidity + "%").c_str());
-  lv_obj_align(text_label_humidity, LV_ALIGN_CENTER, 70, 20);
+  lv_obj_align(text_label_humidity, LV_ALIGN_CENTER, 95, 15);
   lv_obj_set_style_text_font((lv_obj_t*) text_label_humidity, &lv_font_montserrat_22, 0);
 
-  text_label_weather_description = lv_label_create(lv_screen_active());
-  lv_label_set_text(text_label_weather_description, weather_description.c_str());
-  lv_obj_align(text_label_weather_description, LV_ALIGN_BOTTOM_MID, 0, -40);
-  lv_obj_set_style_text_font((lv_obj_t*) text_label_weather_description, &lv_font_montserrat_18, 0);
+  // Monoxide Icon
+  lv_obj_t * icon_monoxide = lv_image_create(lv_screen_active());
+  lv_image_set_src(icon_monoxide, &image_monoxide);  // reemplazá con tu imagen
+  lv_obj_align(icon_monoxide, LV_ALIGN_CENTER, 30, 80);
+
+  text_label_ppm = lv_label_create(lv_screen_active());
+  lv_label_set_text(text_label_ppm, String("   " + monoxide + " ppm").c_str());  // valor fijo por ahora
+  lv_obj_align(text_label_ppm, LV_ALIGN_CENTER, 120, 80);
+  lv_obj_set_style_text_font((lv_obj_t*) text_label_ppm, &lv_font_montserrat_22, 0);
 
   // Create a text label for the time and timezone aligned center in the bottom of the screen
   text_label_time_location = lv_label_create(lv_screen_active());
-  lv_label_set_text(text_label_time_location, String("Last Update: " + last_weather_update + "  |  " + location).c_str());
+  String datetime_str = get_formatted_datetime() + " | " + location;
+  lv_label_set_text(text_label_time_location, datetime_str.c_str());
   lv_obj_align(text_label_time_location, LV_ALIGN_BOTTOM_MID, 0, -10);
-  lv_obj_set_style_text_font((lv_obj_t*) text_label_time_location, &lv_font_montserrat_12, 0);
-  lv_obj_set_style_text_color((lv_obj_t*) text_label_time_location, lv_palette_main(LV_PALETTE_GREY), 0);
+  lv_obj_set_style_text_font(text_label_time_location, &lv_font_montserrat_12, 0);
+  lv_obj_set_style_text_color(text_label_time_location, lv_palette_main(LV_PALETTE_GREY), 0);
 
   // Create a timer to update the weather data every 30 seconds
   lv_timer_t * timer = lv_timer_create(timer_cb, 30000, NULL);
   lv_timer_ready(timer);
 }
-// Function to get weather data from the API
-// This function fetches the weather data from the Open-Meteo API
+
+// Function to get weather data from the DHT sensor
 void get_weather_data() {
   float t = dht.readTemperature();   // Lee temperatura en °C
   float h = dht.readHumidity();      // Lee humedad en %
+  float m = 35.0; // Simulación de valor de monóxido de carbono (MQ7) en ppm
 
   if (isnan(t) || isnan(h)) {
     Serial.println("Error al leer del sensor DHT22");
@@ -174,14 +184,13 @@ void get_weather_data() {
 
   temperature = String(t, 1);  // Un decimal
   humidity = String(h, 1);
+  monoxide = String(m, 1); // Un decimal
 
   // Simulación de valores por defecto
   is_day = 1;               // 1 = día, 0 = noche (podés usar sensor de luz si querés más adelante)
   weather_code = 0;         // Usamos 0 para "CLEAR SKY"
-  last_weather_update = "Local";  // Texto que muestra el origen de datos
+  //last_weather_update = "Local";  // Texto que muestra el origen de datos
 
-  // Fecha fija o simulada
-  current_date = "2025-08-03";  // Podés actualizar esto con un RTC o NTP en el futuro
 }
 
 /*
@@ -333,9 +342,20 @@ static void timer_cb(lv_timer_t * timer){
   LV_UNUSED(timer);
   get_weather_data();
   get_weather_description(weather_code);
+  
   lv_label_set_text(text_label_date, current_date.c_str());
   lv_label_set_text(text_label_temperature, String("      " + temperature + degree_symbol).c_str());
   lv_label_set_text(text_label_humidity, String("   " + humidity + "%").c_str());
-  lv_label_set_text(text_label_weather_description, weather_description.c_str());
-  lv_label_set_text(text_label_time_location, String("Last Update: " + last_weather_update + "  |  " + location).c_str());
+  lv_label_set_text(text_label_time_location, (get_formatted_datetime() + " | " + location).c_str());
+}
+
+String get_formatted_datetime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    return "00/00/0000 00:00";
+  }
+
+  char buffer[30];
+  strftime(buffer, sizeof(buffer), "%d/%m/%Y %H:%M", &timeinfo);
+  return String(buffer);
 }
